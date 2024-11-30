6a2be7ddd5047d97a85ccd4fd0e45a6837443f8c
Dávid Laurovič <david.laurovic@gmail.com>
2024-11-30 19:04:24 +0200

    feat(core): Base impl for CpmDocument transformation

--- cpm-core/src/main/java/cz/muni/fi/cpm/constants/CpmTypeConstants.java
+++ cpm-core/src/main/java/cz/muni/fi/cpm/constants/CpmTypeConstants.java
@@ -1,5 +1,9 @@
 package cz.muni.fi.cpm.constants;
 
+import cz.muni.fi.cpm.CpmFactory;
+import cz.muni.fi.cpm.ICpmFactory;
+import org.openprovenance.prov.model.Type;
+
 public class CpmTypeConstants {
     public static final String BACKWARD_CONNECTOR = "backwardConnector";
     public static final String FORWARD_CONNECTOR = "forwardConnector";
@@ -7,4 +11,13 @@ public class CpmTypeConstants {
     public static final String MAIN_ACTIVITY = "mainActivity";
     public static final String SENDER_AGENT = "senderAgent";
     public static final String RECEIVER_AGENT = "receiverAgent";
+
+    // TODO lepsi sposob pomocou abstrakcii / cpmfactory use?
+    private static final ICpmFactory cF = new CpmFactory();
+    public static final Type BACKWARD_CONNECTOR_TYPE = cF.newCpmType(BACKWARD_CONNECTOR);
+    public static final Type FORWARD_CONNECTOR_TYPE = cF.newCpmType(FORWARD_CONNECTOR);
+    public static final Type IDENTIFIER_TYPE = cF.newCpmType(IDENTIFIER);
+    public static final Type MAIN_ACTIVITY_TYPE = cF.newCpmType(MAIN_ACTIVITY);
+    public static final Type SENDER_AGENT_TYPE = cF.newCpmType(SENDER_AGENT);
+    public static final Type RECEIVER_AGENT_TYPE = cF.newCpmType(RECEIVER_AGENT);
 }
